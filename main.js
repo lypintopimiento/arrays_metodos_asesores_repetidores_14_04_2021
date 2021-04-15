8153cbde00e5ccdf06cf5cce2296af6674877193
lypintopimiento <lypintopimiento@gmail.com>
2021-04-14 20:57:33 -0500

    arrays_metodos_asesores_repetidores_14_04_2021

---
+++ main.js
@@ -0,0 +1,62 @@
+
+//Crear una lista (Array) usando el metodo fill el rango de la
+//lista puede se opcional, almacenar diferentes animales "Acuaticos,
+//Terrestres, Aerios" (Listas anidadas), pedir al usario cual lista
+//desea ver de los animales usando el metodo slice, motrar unas opciones
+//Para visualizar los datos Ejemplo(ver todos,1-10,5-10), y despues
+//Si desea ver los datos ASD o DES
+
+
+let array = ["A","B","C","D","E","F","G","H"];
+
+
+
+
+
+
+
+
+// let lista = new Array(5).fill('');
+// Object.preventExtensions(lista);
+// lista[3] = "";
+// console.log(lista);
+
+// let array = ["A","B","C","D","E","F","G","H"];
+// array.fill("Miguel",3,7);
+// console.log(array);
+
+
+
+// let lista2 = ["A","B","C","D","E","F","G","H"];
+
+// const copia = lista2.slice(3);
+
+
+// console.log(lista2);
+// console.log(copia);
+
+
+// let listaAsc = ["A","B", "C","D"];
+// listaAsc.reverse();
+// console.log(listaAsc);
+
+
+
+
+
+
+
+
+
+
+// let i = -5, indice = ((array.length-1)+i);
+// while(true){
+//     if(array.keys%2==0){
+//         array.fill("Miguel",i, );
+//         indice--;
+//     }
+// }
+
+
+
+
